a5dd4578c61ff9ced108a2decf8403d1b0bfb9b8
malakhalifa <67878978+malakhalifa@users.noreply.github.com>
2022-11-14 16:01:29 -0500

    Update csv_combiner.py

--- csv_combiner.py
+++ csv_combiner.py
@@ -5,60 +5,37 @@
 
 import sys 
 import csv
+import pandas as pd
 
-# takes in two csv files, returns new csv file (result of combining them)
-def csv_combiner(filename, combined_data):
+output = open('output.csv', 'w+')
 
-    # read csv file 
-    f_input = open(filename, 'r')
-    read_csv = csv.reader(f_input)
-    with f_input: # implicitly closes file
-        for row in read_csv:
-            # edge case - only include 1 header at top 
-            if len(combined_data) >= 1 and (row[0]) in combined_data[0]: 
-                pass
-            else:
-                # if header
-                if (len(combined_data) == 0):
-                    row.append("filename")
-                else:
-                    # add additional column with filename to the row 
-                    row.append(filename) 
-                # append new row
-                combined_data.append(row) 
-
-
-# find number of arguments
 n = len(sys.argv)
-combined_data = [] 
-
-# edge case - handling multiple csv files
 for i in range(1, n):
-    # call combiner function
-    csv_combiner(sys.argv[i], combined_data)
+    # safety checks - check that theres a next file and that min 2 files are given
+    if (i >= n-1 or n < 2):
+        break
+    # create dataframes
+    dfA = pd.read_csv(sys.argv[i])
+    dfB = pd.read_csv(sys.argv[i+1])
 
-# create new csv file with combined results 
-with open('combined.csv', 'w+') as f_output:
-    for row in (combined_data):
-        for cell in (row):
-            # remove comma at end of each row 
-            if (cell == row[-1]):
-                f_output.write("\"" + cell + "\"")
-            else:
-                # write to file 
-                f_output.write("\"" + cell + "\"" + ",")
-                # print to std ouput
-                print (cell)
-        f_output.write("\n")
+    # find lengths of dataframes
+    nA = len(dfA.index)
+    nB = len(dfB.index)
 
-    # make sure it's printing well to std output 
-    # edge case - handling different columns 
+    # merge common columns of 2 dataframes
+    df = pd.merge(dfA, dfB, on=list(set(dfA.columns) & set(dfB.columns)), how = "outer")
 
-    # test with: multiple csv files - should work 
+    # keep columns that exist in dfA
+    # df = df[dfA.columns]
 
+    df["filename"] = nA * [sys.argv[i]] + nB * [sys.argv[i+1]]
 
+    # save to new csv
+    df.to_csv("output.csv", mode ='a', index = False)
 
-
-
-
-
+# print new csv file with combined results     
+f_input = open('output.csv', 'r')
+with f_input: # implicitly closes file 
+    reader = csv.reader(f_input)
+    for row in reader:
+        print(row)
